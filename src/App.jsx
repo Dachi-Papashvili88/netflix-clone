d3121274f54869f6cf0db456d46955d5b7b99574
dachi papashvili <115042460+Dachi-Papashvili88@users.noreply.github.com>
2023-10-16 21:11:06 +0000

    API setup and fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import {Routes, Route} from 'react-router-dom'
 import './index.css'
-
+import Home from "./pages/Home"
 import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-    <div>
+    <>
     <Navbar />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+    </>
   )
 }
 
