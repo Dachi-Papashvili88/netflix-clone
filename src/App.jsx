228f5191cf5eb575f95b1a4379f69e3c0ab3f6cf
dachi Papashvili <dachi123.dp@gmail.com>
2023-10-19 02:12:19 +0400

    create signup, login and account pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import "./index.css";
-import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import { AuthContextProvider } from "./context/AuthContext";
+import Home from "./pages/Home";
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import Account from "./pages/Account"
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/account" element={<Account/>}/>
         </Routes>
       </AuthContextProvider>
     </>
