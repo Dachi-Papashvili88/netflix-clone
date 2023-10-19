f5b605a8f3cac5ef34deba87e6b81fa772de5820
dachi papashvili <115042460+Dachi-Papashvili88@users.noreply.github.com>
2023-10-19 22:25:32 +0000

    create protected route

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import "./index.css";
 import Navbar from "./components/Navbar";
 import { AuthContextProvider } from "./context/AuthContext";
 import Home from "./pages/Home";
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
-import Account from "./pages/Account"
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -14,9 +15,16 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/signup" element={<Signup/>}/>
-          <Route path="/account" element={<Account/>}/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/account"
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </AuthContextProvider>
     </>
